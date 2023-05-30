331bf48f2c52a1338d711e4258a2218528dafbf8
MBennettLowe <mbennettlowe@gmail.com>
2023-05-30 15:45:29 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Another React Update
+          EB Enviroment Health is Suspended
         </p>
         <a
           className="App-link"
