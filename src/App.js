17e5f2dc65321b07b676875a016efb2357c7a0a2
Monique Bennett-Lowe <mbennettlowe@gmail.com>
2023-05-30 15:49:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Another React Update
+          Docker Another React Update
         </p>
         <a
           className="App-link"
