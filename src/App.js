3ea54fc9db7ddf01e974f1d727dd1b488df55627
MBennettLowe <mbennettlowe@gmail.com>
2023-05-31 09:17:31 -0400

    Feature Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EB Enviroment Health is Suspended
+          I was changed on the feature branch
         </p>
         <a
           className="App-link"
